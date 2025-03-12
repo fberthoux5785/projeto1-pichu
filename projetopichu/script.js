41d6bceae3bf74b838c50ef911101e2bf6bdf24b
FBERTHOUX <10322325@mackenzista.com.br>
2025-03-12 16:02:37 -0300

    Delete projetopichu directory

--- projetopichu/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('dark-mode-toggle').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-});
-
-function iniciarDesafio() {
-    document.getElementById('resultado').innerHTML = "Desafio iniciado! Resolva o problema...";
-}
